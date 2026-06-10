d42dcd4e546f3d1a2ecec4130d67b776f62cd2aa
Sebu1166 <sebuhi.ramazanov.78@mail.ru>
2026-06-11 01:17:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Languages loaded");
+function setLanguage(lang) {
+    const title = document.getElementById("title");
+
+    if (lang === "en") {
+        title.innerText = "PREMIUM VIP SIGNALS";
+    }
+
+    if (lang === "ru") {
+        title.innerText = "ПРЕМИУМ VIP СИГНАЛЫ";
+    }
+
+    if (lang === "tr") {
+        title.innerText = "PREMIUM VIP SİNYALLER";
+    }
+}
