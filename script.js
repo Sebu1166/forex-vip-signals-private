d89a1f2f1a41d85ff9aae6617c03e3594dbad58d
Sebu1166 <sebuhi.ramazanov.78@mail.ru>
2026-06-11 19:28:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ const timeframes = document.getElementById("timeframes");
     title.innerText = "PREMIUM VIP SIGNALS";
     vipButton.innerText = "GET VIP ACCESS";
 }
-
+markets.innerText = "Forex • Gold • Oil • Crypto";
+timeframes.innerText = "M5 (Gold) • M15 • M30 • H1 • H4";
 if (lang === "ru") {
     title.innerText = "ПРЕМИУМ VIP СИГНАЛЫ";
     vipButton.innerText = "ПОЛУЧИТЬ VIP ДОСТУП";
 }
-
+markets.innerText = "Форекс • Золото • Нефть • Криптовалюта";
+timeframes.innerText = "M5 (Золото) • M15 • M30 • H1 • H4";
 if (lang === "tr") {
     title.innerText = "PREMIUM VIP SİNYALLER";
     vipButton.innerText = "VIP ERİŞİM AL";
 }
-}
+markets.innerText = "Forex • Altın • Petrol • Kripto";
+timeframes.innerText = "M5 (Altın) • M15 • M30 • H1 • H4";
