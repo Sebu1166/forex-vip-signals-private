37b2562a5fdcfe15fe174336a6bb529cd43ae335
Sebu1166 <sebuhi.ramazanov.78@mail.ru>
2026-06-11 18:53:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ const vipButton = document.getElementById("vipButton");
         title.innerText = "PREMIUM VIP SIGNALS";
     }
 
-    if (lang === "ru") {
-        title.innerText = "ПРЕМИУМ VIP СИГНАЛЫ";
-    }
+    if (lang === "en") {
+    title.innerText = "PREMIUM VIP SIGNALS";
+    vipButton.innerText = "GET VIP ACCESS";
+}
 
-    if (lang === "tr") {
-        title.innerText = "PREMIUM VIP SİNYALLER";
-    }
+if (lang === "ru") {
+    title.innerText = "ПРЕМИУМ VIP СИГНАЛЫ";
+    vipButton.innerText = "ПОЛУЧИТЬ VIP ДОСТУП";
+}
+
+if (lang === "tr") {
+    title.innerText = "PREMIUM VIP SİNYALLER";
+    vipButton.innerText = "VIP ERİŞİM AL";
+}
 }
